0377f813afe1e586ec95034989aded8e1543e90c
Márcia Oliveira <marciaoliveira@MacBook-Pro-de-Marcia.local>
2023-09-18 12:06:28 +0100

    changes

--- src/si/__init__.py
+++ src/si/__init__.py
@@ -1,9 +1,9 @@
-__author__ = "João Correia"
+__author__ = "Márcia Oliveira"
 __credits__ = ["João Correia, Fernando Cruz", "Vítor Pereira"]
 __license__ = "Apache License 2.0"
 __version__ = "0.0.1"
-__maintainer__ = "João Correia"
-__email__ = "jfscorreia95@gmail.com"
+__maintainer__ = "Márcia Oliveira"
+__email__ = "marcia27999@gmail.com"
 
 # -----------------------------------------------------------------------------------
 # This package is heavily inspired and adapted from
